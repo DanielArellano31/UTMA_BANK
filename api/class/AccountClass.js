0287db3eafdb963ed42bf07b80decebb6ff11251
DanielArellano31 <utm23090410@utma.edu.mx>
2024-07-25 13:49:13 -0600

    25/07/24

---
+++ api/class/AccountClass.js
@@ -0,0 +1,81 @@
+import AccountModel from "../models/AccountModel.js"
+
+class ManagerAccounts{
+    constructor(userId, accountNumber, accountType, balance){
+        this.userId= userId;
+        this.accountNumber= accountNumber;
+        this.accountType= accountType;
+        this.balance= balance;
+        
+    }
+    //obtener cuentas
+    async getAccounts(){
+        try {
+            const accounts =  await AccountModel.find();
+            return accounts;
+            
+        } catch (error) {
+            throw new Error (`Error al obtener cuentas: ${error}`);
+        }
+        
+    }
+    
+    //obtener cuenta
+    async getAccount (id){
+        try {
+            const account = await AccountModel.findById();
+            return account;
+        } catch (error) {
+            throw new Error (`Error al obtener cuentas: ${error}`);
+            
+        }
+    }
+    //agregar BALANCE
+    async addBalance(id, ammount){
+        try {
+            this.balance += ammount;
+            await AccountModel.findByIdAndUpdate(id,{
+                $set:{
+                    balance: this.balance
+                }
+            });
+            return "Ok";
+        } catch (error) {
+            throw new Error (`Error al agregar monto: ${error}`);            
+        }
+        
+    }
+    //restar  BALANCE
+    async restBalance(id, ammount){
+        try {
+            this.balance -= ammount;
+            await AccountModel.findByIdAndUpdate(id,{
+                $set:{
+                    balance: this.balance
+                }
+            });
+            return "Ok";
+        } catch (error) {
+            throw new Error (`Error al restar monto: ${error}`);            
+        }
+        
+    }
+    
+    //CREAR UNA CUENTA
+    async createAccount(){
+        try {
+            await AccountModel.create({
+                userId:this.userId,
+                accountNumber:this.accountNumber,
+                accountType:this.accountType,
+                balance:this.balance
+            });
+            return "Ok"
+        } catch (error) {
+            throw new Error (`Error al crear cuenta: ${error}`);            
+
+        }
+    }
+
+}
+export default ManagerAccounts;
---
+++ api/class/CardClass.js
@@ -0,0 +1,56 @@
+// CREAR UNA TARJETA
+//OBTENER UNA TARJETA
+//OBTENER TARJETAS
+import CardModel from "../models/CardModel.js";
+
+class ManagerCard{
+    constructor( userId,accountId,cardNumber,cardType,expirationDate,securityCode,status){
+        this.userId = userIdstatus;
+        this.accountId = accountId;
+        this.cardNumber = cardNumber;
+        this.cardType = cardType;
+        this.expirationDate = expirationDate;
+        this.securityCode = securityCode;
+        this.status = status;
+
+    }
+    async createCard(){
+        try {
+            await CardModel.create({
+                userIdstatus:this.userId,
+                accountId:this.accountId,
+                cardNumber:this.cardNumber,
+                cardType:this.cardType,
+                expirationDate:this.expirationDate,
+                securityCode:this.securityCode,
+                status:this.status,
+       
+               });
+       return "Ok";
+        } catch (error) {
+            throw new Error (`Error al crear tarjeta: ${error}`);
+        }
+    }
+       async getCards(){
+        try {
+            const cards =  await CardModelModel.find();
+            return cards;
+            
+        } catch (error) {
+            throw new Error (`Error al obtener cuentas: ${error}`);
+        }
+        
+    }
+    
+    async getCard (id){
+        try {
+            const card = await CardModel.findById();
+            return card;
+        } catch (error) {
+            throw new Error (`Error al obtener cuentas: ${error}`);
+            
+        }
+    }
+}
+
+export default ManagerCard;ñ
